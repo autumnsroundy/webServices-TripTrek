e302be586d7fd50ede3ad7945464b495d830497a
melanyvlm <melany.svalencia@gmail.com>
2025-11-17 13:38:18 -0500

    mongoose schemas

--- server.js
+++ server.js
@@ -13,12 +13,53 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
+// Import models 
+const { User, Trip, Destination, Activity } = require('./models/models');
+
 // === Basic route ===
 app.get('/', (req, res) => {
   res.send('Hello, TripTrek!');
 });
 
 // === Start Server ===
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Test Rute to create an user 
+app.post('/api/users', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ 
+      success: true, 
+      message: 'User created successfully', 
+      data: user 
+    });
+  } catch (error) {
+    res.status(400).json({ 
+      success: false, 
+      message: error.message 
+    });
+  }
+});
+
+// Rute to obtain all users 
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({ 
+      success: true, 
+      data: users 
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      success: false, 
+      message: error.message 
+    });
+  }
+});
+
+//  Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
