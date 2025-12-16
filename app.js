67274ea3eccfa83f93f5b06f753e2435e2a300ae
autumnsroundy <rou24001@byui.edu>
2025-12-15 16:25:12 -0900

    Auth 2.0 trust proxy

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const utilities = require('./utilities');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
